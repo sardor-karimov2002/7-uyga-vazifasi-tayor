d47c338d9ed005d44d5193aa0df641a5e8fc869d
sardor-karimov2002 <sardorbekkarimov155@gmail.com>
2022-01-10 19:36:39 +0500

    tayor  bo'ldi

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
+const dollir_sped=10800
+const rubl_sped=10354.03
+const euro_sped=160
 var form = document.querySelector('#calc')
 var fristVar=document.getElementById('first')
 var secondVar=document.getElementById('second')
 var resultSpan=document.getElementById('result')
 
-secondVar.addEventListener('change',function(event){
-    event.preventDefault()
-    resultSpan.textContent=fristVar.value/100
 
-})
 
 form.addEventListener('submit',function(event){
-    event.preventDefault()
-
-    resultSpan.textContent=fristVar.value/10354.03
+    
+    firstVarValue = fristVar.value - 0
+    event.preventDefault();
+    console.log(firstVarValue   )
+    if(secondVar.value==='USD'){
+        resultSpan.textContent= convert(firstVarValue,dollir_sped)
+        console.log(convert(firstVarValue,dollir_sped))
+    }
+    if(secondVar.value==='RUB'){
+        resultSpan.textContent= convert(firstVarValue,rubl_sped)
+    }
+    if(secondVar.value==='EUR'){
+        resultSpan.textContent= convert(firstVarValue,euro_sped)
+    }
 })
 
+function convert(firstValue ,cur){
+    return firstValue/cur
+}
 
-euro.addEventListener('keyup',function(event){
-    event.preventDefault()
-    resultSpan.textContent=secondVar.value/160
 
-})
 
